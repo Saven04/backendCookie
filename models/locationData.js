505b2c5ed477c2be0a06f3d7b0e3416366bb906a
Savenx20 <22337044@student.hindustanuniv.ac.in>
2025-02-12 18:35:20 +0530

    Update locationData.js

--- models/locationData.js
+++ models/locationData.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const locationSchema = new mongoose.Schema({
+const locationSchema = new mongoose.Schema(
+  {
     consentId: { type: String, required: true },
     ipAddress: { type: String, required: true },
     isp: { type: String, required: true },
@@ -8,7 +9,10 @@ const locationSchema = new mongoose.Schema({
     country: { type: String, required: true },
     latitude: { type: Number },
     longitude: { type: Number },
-});
+    createdAt: { type: Date, default: Date.now, expires: "30d" }, // ⏳ Auto-delete after 30 days
+  },
+  { timestamps: true }
+);
 
 const Location = mongoose.model("Location", locationSchema);
 

7e5d76cdd7ba5dc2d5fb52a47c59caae7e0fd071
Savenx20 <22337044@student.hindustanuniv.ac.in>
2025-03-17 08:56:00 +0530

    Update AdminRoutes.js

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -4,7 +4,6 @@ const CookiePreferences = require('../models/cookiePreference');
 const Location = require('../models/locationData');
 const User = require('../models/user');
 
-// Get all GDPR data
 router.get('/gdpr-data', async (req, res) => {
     try {
         const data = await CookiePreferences.aggregate([
@@ -28,8 +27,15 @@ router.get('/gdpr-data', async (req, res) => {
                 $project: {
                     consentId: 1,
                     preferences: 1,
-                    timestamp: 1,
+                    'timestamps.cookiePreferences': {
+                        createdAt: '$createdAt',
+                        updatedAt: '$updatedAt'
+                    },
                     ipAddress: { $arrayElemAt: ['$location.ipAddress', 0] },
+                    'timestamps.location': {
+                        createdAt: { $arrayElemAt: ['$location.createdAt', 0] },
+                        updatedAt: { $arrayElemAt: ['$location.updatedAt', 0] }
+                    },
                     username: { $arrayElemAt: ['$user.username', 0] }
                 }
             }
@@ -40,7 +46,6 @@ router.get('/gdpr-data', async (req, res) => {
     }
 });
 
-// Search by consentId
 router.get('/gdpr-data/:consentId', async (req, res) => {
     const { consentId } = req.params;
     try {
@@ -66,8 +71,15 @@ router.get('/gdpr-data/:consentId', async (req, res) => {
                 $project: {
                     consentId: 1,
                     preferences: 1,
-                    timestamp: 1,
+                    'timestamps.cookiePreferences': {
+                        createdAt: '$createdAt',
+                        updatedAt: '$updatedAt'
+                    },
                     ipAddress: { $arrayElemAt: ['$location.ipAddress', 0] },
+                    'timestamps.location': {
+                        createdAt: { $arrayElemAt: ['$location.createdAt', 0] },
+                        updatedAt: { $arrayElemAt: ['$location.updatedAt', 0] }
+                    },
                     username: { $arrayElemAt: ['$user.username', 0] }
                 }
             }
